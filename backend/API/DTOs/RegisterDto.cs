907b4c0b7c8699771d3f5bfcba6db996fd3ade24
idanyossi <146638614+idanyossi@users.noreply.github.com>
2026-01-01 18:28:08 +0200

    initialized .net9 backend

---
+++ backend/API/DTOs/RegisterDto.cs
@@ -0,0 +1,7 @@
+
+public interface RegisterDTO
+{
+    string Username { get; set; }
+    string Password { get; set; }
+    string Role { get; set; }
+}
---
+++ backend/API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Asset> Assets { get; set; }
+    }
+}
